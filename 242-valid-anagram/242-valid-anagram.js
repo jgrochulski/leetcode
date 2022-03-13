ba411df298cd0c2fe3ddfc3dc39eea41c3af0fdd
Jan Grochulski <janek.grochulski@gmail.com>
2022-03-12 21:45:43 -0600

    Time: 102 ms (69.05%), Space: 42.4 MB (79.65%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.js
+++ 242-valid-anagram/242-valid-anagram.js
@@ -7,29 +7,16 @@ var isAnagram = function(s, t) {
   
   if (s.length != t.length) return false
   
-  const sMap = new Map()
-  const tMap = new Map()
-  // let ans = true;
+  const chars = new Array(26).fill(0)
   
-  for (let i = 0; i < t.length; i++) {
-    const tCount = tMap.has(t[i]) ? tMap.get(t[i]) : 0
-    tMap.set(t[i], tCount + 1)
-    
-    const sCount = sMap.has(s[i]) ? sMap.get(s[i]) : 0
-    sMap.set(s[i], sCount + 1)
+  for (let i = 0; i < s.length; i++) {
+    chars[s.charCodeAt(i) - 'a'.charCodeAt(0)] += 1
+    chars[t.charCodeAt(i) - 'a'.charCodeAt(0)] -= 1
   }
   
-  for (const [key, value] of sMap) {
-    if (tMap.get(key) != value) return false
+  for (let i = 0; i < chars.length; i++) {
+    if (chars[i] != 0) return false
   }
-  
-  // sMap.forEach((value, key) => {
-  //   console.log(key + " : " + value)
-  //   if (tMap.get(key) != value) {
-  //     console.log("false!")
-  //     ans = false
-  //   }
-  // })
   
   return true
   
