f88c89e053a454fb67b86aa0ec60ed5911bf9cb0
Jan Grochulski <janek.grochulski@gmail.com>
2022-03-12 20:44:01 -0600

    Time: 96 ms (76.61%), Space: 43.5 MB (60.90%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,35 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+  
+  if (s.length != t.length) return false
+  
+  const sMap = new Map()
+  const tMap = new Map()
+  let ans = true;
+  
+  for (let i = 0; i < t.length; i++) {
+    const tCount = tMap.has(t[i]) ? tMap.get(t[i]) : 0
+    tMap.set(t[i], tCount + 1)
+    
+    const sCount = sMap.has(s[i]) ? sMap.get(s[i]) : 0
+    sMap.set(s[i], sCount + 1)
+  }
+  
+  // console.log(tMap)
+  // console.log(sMap)
+  
+  sMap.forEach((value, key) => {
+    console.log(key + " : " + value)
+    if (tMap.get(key) != value) {
+      console.log("false!")
+      ans = false
+    }
+  })
+  
+  return ans
+  
+};
