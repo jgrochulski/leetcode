63f88386a2b9f9c6a648ac6b27c91367f4128876
Jan Grochulski <janek.grochulski@gmail.com>
2022-03-21 00:19:53 -0500

    Time: 171 ms (38.97%), Space: 52 MB (9.02%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.js
@@ -0,0 +1,36 @@
+/**
+ * @param {number[][]} intervals
+ * @return {number[][]}
+ */
+var merge = function(intervals1) {
+  
+  if (intervals1.legnth === 1) return intervals
+  
+  const intervals = intervals1.sort((a, b) => {
+    return a[0] - b[0]
+  })
+  
+  console.log(intervals)
+  
+  let start = intervals[0][0]
+  let end = intervals[0][1]
+  
+  const left = []
+  const right = []
+  
+  for (let i = 1; i < intervals.length; i++) {
+    if (end >= intervals[i][0]) {
+      end = Math.max(end, intervals[i][1])
+    }
+    else {
+      left.push([start, end])
+      start = intervals[i][0]
+      end = intervals[i][1]
+    }
+  }
+  left.push[start, end]
+  
+  return [...left, [start, end]]
+  console.log([start, end])
+  
+};
