50f89266ad489892d6fc01e7c6a8d0b8ecd09052
Jan Grochulski <janek.grochulski@gmail.com>
2022-03-23 01:09:51 -0500

    Time: 153 ms (5.23%), Space: 45.8 MB (16.80%) - LeetHub

---
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,45 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var maxDepth = function(node) {
+  if (!node) return 0
+  
+  else {
+    let lMax = maxDepth(node.left)
+    let rMax = maxDepth(node.right)
+    
+    if (lMax > rMax) {
+      return lMax + 1
+    }
+    else {
+      return rMax + 1
+    }
+  }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
