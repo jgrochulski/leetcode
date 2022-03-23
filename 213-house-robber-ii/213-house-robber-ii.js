270879828f907cc8bdfbb7b03612dbaa7fb0b2be
Jan Grochulski <janek.grochulski@gmail.com>
2022-03-23 17:23:44 -0500

    Time: 77 ms (58.09%), Space: 42.3 MB (38.67%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.js
+++ 213-house-robber-ii/213-house-robber-ii.js
@@ -6,30 +6,19 @@ var rob = function(nums) {
   const len = nums.length
   
   if (len === 1) return nums[0]
-
-  let rob1 = 0
-  let rob2 = 0
   
   let ans = 0
   
-  for (let i = 0; i < len - 1; i++) {
-    const current = Math.max(nums[i] + rob1, rob2)
-    rob1 = rob2
-    rob2 = current
+  for (let i = 0; i < 2; i++) {
+    let rob1 = 0
+    let rob2 = 0
+    for (let j = i; j < len - 1 + i; j++) {
+      const current = Math.max(nums[j] + rob1, rob2)
+      rob1 = rob2
+      rob2 = current
+    }
+    ans = Math.max(ans, rob2)
   }
-  
-  ans = Math.max(ans, rob2)
-  
-  rob1 = 0
-  rob2 = 0
-  
-  for (let i = 1; i < len; i++) {
-    const current = Math.max(nums[i] + rob1, rob2)
-    rob1 = rob2
-    rob2 = current
-  }
-  
-  ans = Math.max(ans, rob2)
   
   return ans
   
