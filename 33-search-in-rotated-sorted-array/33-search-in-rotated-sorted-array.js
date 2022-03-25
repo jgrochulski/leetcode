fb082c7efe9f80bff18c9064c5449bd421281de9
Jan Grochulski <janek.grochulski@gmail.com>
2022-03-25 00:21:28 -0500

    Time: 111 ms (15.78%), Space: 44 MB (7.24%) - LeetHub

---
+++ 33-search-in-rotated-sorted-array/33-search-in-rotated-sorted-array.js
@@ -0,0 +1,53 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  if (nums.length === 1) {
+    return nums[0] === target ? 0 : -1
+  }
+  
+  const len = nums.length
+  let l = 0
+  let r = len - 1
+  let m = 0
+  
+  // console.log("l:" + l + " m:" + m + " r:" + r)
+  // console.log("***********")
+    console.log("nums[l]:" + nums[l] + " nums[m]:" + nums[m] + " nums[r]:" + nums[r])
+  
+  
+  while (l <= r) {
+    
+    m = Math.floor((l + r) / 2)
+
+    if (nums[m] === target) return m
+    
+    if (nums[l] <= nums[m]){ // left sorted
+      if (nums[l] <= target && nums[m] > target) { // look left
+        r = m - 1
+        console.log("case1")
+      }
+      else { // look right
+        l = m + 1
+        console.log("case2")
+      }
+    }
+    else {  // right sorted
+      if (nums[m] < target && nums[r] >= target) { // look right
+        l = m + 1
+        console.log("case3")
+      }
+      else { // look left
+        r = m - 1
+        console.log("case4")
+      }
+    }   
+  }
+  return -1
+};
+  
+  //  [4,5,6,7,0,1,2] -> 4
+  
+  
