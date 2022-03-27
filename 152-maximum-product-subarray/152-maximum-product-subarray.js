706326db003ece56a804d36eb181c992cc8c6a9b
Jan Grochulski <janek.grochulski@gmail.com>
2022-03-26 22:38:52 -0500

    Time: 104 ms (31.32%), Space: 42.4 MB (79.76%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.js
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -4,34 +4,34 @@
  */
 var maxProduct = function(nums) {
   
-    if (nums.length === 1) return nums[0];
-    if (Array.isArray(nums) && !nums.length) return 0;
-
-    let currentMax = nums[0];
-    let currentMin = nums[0];
-    let previousMax = nums[0];
-    let previousMin = nums[0];
-    let solution = currentMax; // 2
-
-    for (let i = 1; i < nums.length; i++) {
-        let currentNumber = nums[i];
-        currentMax = Math.max(currentNumber, currentNumber * previousMax, currentNumber * previousMin);
-        //  3   6   6 ->   6
-        // -2 -12  -6 ->  -2
-        //  4  -8 -48 ->   4
-        currentMin = Math.min(currentNumber, currentNumber * previousMax, currentNumber * previousMin);
-        //  3   6   6 ->   3
-        // -2 -12  -6 -> -12
-        //  4  -8 -48 -> -48
-        previousMax = currentMax;
-        previousMin = currentMin;
-
-        
-        solution = Math.max(solution, currentMax);
-        // 6, 6, 6 -> 6
-    }
-    return solution;
+  if (nums.length === 1) return nums[0];
   
+  let currentMax = nums[0]
+  let currentMin = nums[0]
+  let previousMax = nums[0]
+  let previousMin  = nums[0]
   
+  let solution = currentMax
+  
+  for (let i = 1; i < nums.length; i++) {
+    
+    const currentNumber = nums[i]
+    
+    currentMax = Math.max(
+      currentNumber,
+      currentNumber * previousMax,
+      currentNumber * previousMin
+    )
+    currentMin = Math.min(
+      currentNumber,
+      currentNumber * previousMax,
+      currentNumber * previousMin
+    )
+    previousMax = currentMax
+    previousMin = currentMin
+    
+    solution = Math.max(solution, currentMax)
+  }
+  return solution
 };
 
