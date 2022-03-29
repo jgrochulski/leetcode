bb9c60b36b358cd991353e4c04376292c66916f6
Jan Grochulski <janek.grochulski@gmail.com>
2022-03-28 23:54:29 -0500

    Time: 108 ms (62.76%), Space: 51 MB (13.82%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.js
@@ -0,0 +1,52 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+  if (numCourses === 1 || prerequisites.length === 0) return true
+
+  const map = {}
+  
+  for (let i = 0; i <= numCourses; i++) {
+    map[i] = []
+  }
+
+  for (const course of prerequisites) {
+    if (map[course[0]]) {
+      map[course[0]] = [...map[course[0]], course[1]]
+    }
+    else {
+      map[course[0]] = [course[1]]
+    }
+  }
+
+  console.log(map)
+  
+  const set = new Set()
+  
+  function dfs(course) {
+    // console.log(course)
+    if (map[course].length === 0) return true
+    
+    if (set.has(course)) return false
+    
+    set.add(course)
+    
+    for (const prereq of map[course]) {
+      if (!dfs(prereq)) return false
+    }
+    
+    set.delete(course)
+    map[course] = []
+    
+    return true
+  }
+  
+  for (const course in map) {
+    if (!dfs(course)) return false
+  }
+  
+  return true
+    
+};
