3d095a51f17b69aa6a8f078331b270e0afa12b39
Jan Grochulski <janek.grochulski@gmail.com>
2022-04-06 23:14:16 -0500

    Time: 559 ms (60.19%), Space: 82.2 MB (86.69%) - LeetHub

--- 204-count-primes/204-count-primes.js
+++ 204-count-primes/204-count-primes.js
@@ -3,23 +3,23 @@
  * @return {number}
  */
 var countPrimes = function(n) {
-    const arr = new Array(n).fill(true)
-    let count = 0
+    const primes = new Array(n).fill(true)
+    let primeCount = 0
     
-    for (let i = 2; i * i < arr.length; i++) {
-      if (!arr[i]) continue
+    for (let i = 2; i * i < primes.length; i++) {
+      if (!primes[i]) continue
       else {
-        for (let j = i; j * i < arr.length; j++) {
-          arr[i * j] = false
+        for (let j = i; j * i < primes.length; j++) {
+          primes[i * j] = false
         }
       }
     }
   
-  for (let i = 2; i < arr.length; i++) {
-    if (arr[i] === true) {
-      count++
+  for (let i = 2; i < primes.length; i++) {
+    if (primes[i] === true) {
+      primeCount++
     }
   }
   
-  return count
+  return primeCount
 };
