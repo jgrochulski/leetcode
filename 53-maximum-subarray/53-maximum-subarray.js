01789fc9571c459441f5abb95060f97abdbb14fd
Jan Grochulski <janek.grochulski@gmail.com>
2022-04-10 23:46:04 -0500

    Time: 102 ms (63.46%), Space: 50.4 MB (60.76%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -5,13 +5,28 @@
 var maxSubArray = function(nums) {
   let n = nums.length
   let max = nums[0] // handle [-1] case
-  let dp = []
-  dp[0] = nums[0]
+//   let dp = []
+//   dp[0] = nums[0]
   
-  for (let i = 0; i < n; i++) {
-    dp[i] = nums[i] + (dp[i-1] > 0 ? dp[i-1] : 0)
-    max = dp[i] > max ? dp[i] : max
+//   for (let i = 1; i < n; i++) {
+//     dp[i] = nums[i] + (dp[i-1] > 0 ? dp[i-1] : 0)
+//     max = dp[i] > max ? dp[i] : max
+//   }
+//   return max
+  
+   let l = 0
+   let r = 0
+   
+   let sum = nums[0]
+   while (r < n) {
+     if (nums[r] > sum) {
+       l = r
+       sum = nums[r]
+     }
+     max = Math.max(max, sum)
+     r++
+     sum = sum + nums[r]
   }
-  return max
- };
+  return max  
+};
 
