69e7a63ee27fbbd846f35b81b89a9940376acdd9
Jan Grochulski <janek.grochulski@gmail.com>
2022-05-16 18:52:29 -0500

    Time: 72 ms (73.90%), Space: 42.2 MB (93.22%) - LeetHub

--- 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.js
+++ 674-longest-continuous-increasing-subsequence/674-longest-continuous-increasing-subsequence.js
@@ -4,14 +4,26 @@
  */
 var findLengthOfLCIS = function(nums) {
   
-  let ans = 0;
-  let anchor = 0;
+//   let ans = 0;
+//   let anchor = 0;
   
-  for (let i = 0; i < nums.length; i++) {
-    if (i > 0 && nums[i] <= nums[i - 1]) {
-      anchor = i
-    }
-    ans = Math.max(ans, i - anchor + 1)
+//   for (let i = 0; i < nums.length; i++) {
+//     if (i > 0 && nums[i] <= nums[i - 1]) {
+//       anchor = i
+//     }
+//     ans = Math.max(ans, i - anchor + 1)
+//   }
+  
+//   return ans
+  
+  let l = 0
+  let r = 0
+  let ans = 0
+  
+  while (r < nums.length) {
+    if (nums[r] <= nums[r-1]) l = r
+    r++
+    ans = Math.max(ans, r - l)
   }
   
   return ans
